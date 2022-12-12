528454d028cb589740adf05a68255d5f41ef97aa
Kevin <100741086+keppinmind@users.noreply.github.com>
2022-12-12 21:58:44 +0700

    aset

--- resources/views/tampilan/service.blade.php
+++ resources/views/tampilan/service.blade.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-          rel="stylesheet" 
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
-          crossorigin="anonymous">
-    <title>Service</title>
-</head>
-<body>
+@extends('layouts.halaman')
+@section('title', 'Service')
+@section('style')
+@endsection
+@section('isi')
     <div class="mt-5">
         <h1 class="text-center" style="font-size: 40px;">SERVICE</h1>
+        <img src="{{ asset('app/assets/image/aboutus.png') }}" />
     </div>
-</body>
-</html>
+@endsection
